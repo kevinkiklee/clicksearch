457634d88b5e1b0d1384fb66a38169cc0ed08f14
Kevin K. Lee <kevin.kik.lee@gmail.com>
2017-11-25 12:37:14 -0500

    Send URL message to the background

--- content.js
+++ content.js
@@ -1,10 +1,11 @@
-/* eslint-disable no-console */
-console.log('CONTENT SCRIPT!!!')
-
 var body = document.querySelector('body')
 
 body.addEventListener('click', function(event) {
 	if (event.metaKey) {
-		console.log('META!!!')
+		var selectedString = window.getSelection().toString()
+		var queryString = selectedString.split(' ').join('+')
+		var url = 'https://www.google.com/search?q=' + queryString
+
+		chrome.runtime.sendMessage({ url: url })
 	}
 })
