fb682cea8314fb33db7e5e768b50c869e9300924
Kevin K. Lee <kevin.kik.lee@gmail.com>
2017-11-25 13:53:11 -0500

    Convert tabs to spaces

--- content.js
+++ content.js
@@ -1,18 +1,18 @@
 var body = document.querySelector('body')
 
 body.addEventListener('click', function(event) {
-	if (event.metaKey || event.ctrlKey) {
-		var selectedString = window.getSelection().toString()
+  if (event.metaKey || event.ctrlKey) {
+    var selectedString = window.getSelection().toString()
 
-		if (selectedString) {
-			var shouldFocus = event.shiftKey
-			var queryString = selectedString.split(' ').join('+')
-			var url = 'https://www.google.com/search?q=' + queryString
+    if (selectedString) {
+      var shouldFocus = event.shiftKey
+      var queryString = selectedString.split(' ').join('+')
+      var url = 'https://www.google.com/search?q=' + queryString
 
-			chrome.runtime.sendMessage({
-				url: url,
-				shouldFocus: shouldFocus
-			})
-		}
-	}
+      chrome.runtime.sendMessage({
+        url: url,
+        shouldFocus: shouldFocus
+      })
+    }
+  }
 })
