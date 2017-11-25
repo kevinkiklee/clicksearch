ad73b78ad504ff0acde53a9adff821e628b0dc1f
Kevin K. Lee <kevin.kik.lee@gmail.com>
2017-11-24 22:22:38 -0500

    Setup Webpack

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    background: './lib/background.js',
+    inject: './src/inject.js'
+  },
+  output: {
+    filename: '[name].js',
+    path: __dirname + '/'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['env']
+        }
+      }
+    ]
+  },
+  stats: {
+    colors: true
+  },
+  devtool: 'source-map'
+};
