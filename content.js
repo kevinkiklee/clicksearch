894191b74da9f51814183b0a861932d1e566d923
Kevin K. Lee <kevin.kik.lee@gmail.com>
2017-11-25 13:41:32 -0500

    Add check for empty selection

--- content.js
+++ content.js
@@ -2,15 +2,17 @@ var body = document.querySelector('body')
 
 body.addEventListener('click', function(event) {
 	if (event.metaKey || event.ctrlKey) {
-		var shouldFocus = event.shiftKey
-
 		var selectedString = window.getSelection().toString()
-		var queryString = selectedString.split(' ').join('+')
-		var url = 'https://www.google.com/search?q=' + queryString
 
-		chrome.runtime.sendMessage({
-			url: url,
-			shouldFocus: shouldFocus
-		})
+		if (selectedString) {
+			var shouldFocus = event.shiftKey
+			var queryString = selectedString.split(' ').join('+')
+			var url = 'https://www.google.com/search?q=' + queryString
+
+			chrome.runtime.sendMessage({
+				url: url,
+				shouldFocus: shouldFocus
+			})
+		}
 	}
 })
