0907ae67d912ab2a3c4c61710815f3febaaeff7b
Kevin K. Lee <kevin.kik.lee@gmail.com>
2017-11-25 11:51:27 -0500

    Add metaKey+click event listener

--- content.js
+++ content.js
@@ -1,1 +1,10 @@
-console.log('CONTENT SCRIPT!!!');
+/* eslint-disable no-console */
+console.log('CONTENT SCRIPT!!!')
+
+var body = document.querySelector('body')
+
+body.addEventListener('click', function(event) {
+	if (event.metaKey) {
+		console.log('META!!!')
+	}
+})
