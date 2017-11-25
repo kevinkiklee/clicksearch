e6ca98a0e9d0f36b2bd162d49a93ef0631e441cb
Kevin K. Lee <kevin.kik.lee@gmail.com>
2017-11-25 12:37:38 -0500

    Add control key support

--- content.js
+++ content.js
@@ -1,7 +1,7 @@
 var body = document.querySelector('body')
 
 body.addEventListener('click', function(event) {
-	if (event.metaKey) {
+	if (event.metaKey || event.ctrlKey) {
 		var selectedString = window.getSelection().toString()
 		var queryString = selectedString.split(' ').join('+')
 		var url = 'https://www.google.com/search?q=' + queryString
